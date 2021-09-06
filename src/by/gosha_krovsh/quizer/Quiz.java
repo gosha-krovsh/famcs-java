56d9db922afb03c6aa52dfddefd48ce9b2f1e377
George Krovsh <gosha.krovsh@gmail.com>
2021-09-06 23:23:33 +0300

    Add enum Result and class Quiz

---
+++ src/by/gosha_krovsh/quizer/Quiz.java
@@ -0,0 +1,76 @@
+package by.gosha_krovsh.quizer;
+
+import java.util.HashMap;
+
+public class Quiz {
+    static HashMap<String, Task> getQuizMap() {
+        // TODO(George) realise after Task and TaskGenerator
+        return new HashMap<String, Task>();
+    }
+
+    Quiz(TaskGenerator generator, int taskCount) {
+        this.generator = generator;
+        this.taskCount = taskCount;
+    }
+
+    Task nextTask() {
+        if (taskCount == 0) {
+            // TODO(George) throw exception
+        }
+
+        if (isInputCorrect) {
+            currentTask = generator.generate();
+            --taskCount;
+        }
+        return currentTask;
+    }
+
+    Result provideAnswer(String answer) {
+        Result result = currentTask.validate(answer);
+        switch (result) {
+            case OK: {
+                isInputCorrect = true;
+                ++correctAnswerNumber;
+            }
+            case WRONG: {
+                isInputCorrect = true;
+                ++wrongAnswerNumber;
+            }
+            case INCORRECT_INPUT: {
+                isInputCorrect = false;
+                ++incorrectInputNumber;
+            }
+        }
+        return result;
+    }
+
+    public double getMark() {
+        return 1. * correctAnswerNumber / (correctAnswerNumber + wrongAnswerNumber);
+    }
+
+    boolean isFinished() {
+        return taskCount == 0;
+    }
+
+    public int getCorrectAnswerNumber() {
+        return correctAnswerNumber;
+    }
+
+    public int getWrongAnswerNumber() {
+        return wrongAnswerNumber;
+    }
+
+    public int getIncorrectInputNumber() {
+        return incorrectInputNumber;
+    }
+
+    private int correctAnswerNumber;
+    private int wrongAnswerNumber;
+    private int incorrectInputNumber;
+
+    private Task currentTask;
+    private boolean isInputCorrect = true;
+
+    private final TaskGenerator generator;
+    private int taskCount;
+}
